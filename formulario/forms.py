ea99a9f6be41614474d9c57c960bc8b77af1e19c
Ariane <arianecamilo.alves@gmail.com>
2020-10-08 16:14:13 -0300

    adds helpers

--- formulario/forms.py
+++ formulario/forms.py
@@ -7,6 +7,7 @@ from .models import Mapeamento
 
 class PostCityForm(forms.ModelForm):
     data_inicial = forms.DateField(label='Qual a data do arquivo mais antigo disponível online?',
+                                    help_text='O município publica diários oficiais na internet desde quando? Informe a data do primeiro diário oficial cujo arquivo é possível visualizar na página que irá cadastrar neste formulário.',
                                    input_formats=settings.DATE_INPUT_FORMATS,
                                    widget=forms.DateInput(attrs={'placeholder':'DD/MM/AAAA'}),
                                    required=False)
@@ -24,4 +25,9 @@ class PostCityForm(forms.ModelForm):
             'fonte_4': 'Informe uma fonte de publicação de arquivos',
             'is_online': 'Existe uma fonte de publicação disponível online?',
             'tipo_arquivo': 'Qual o formato dos arquivos?',
+        }
+        help_texts = {
+            'municipio': 'Digite o nome do município que deseja cadastrar. Se ele não constar na lista disponível, significa que seu mapeamento já foi realizado. Saiba mais <a href="/sobre/#faq-1">aqui</a>.',
+            'is_online': 'É possível encontrar o diário oficial do município publicado em alguma página da internet? Caso não seja possível, clique <a href="sobre/#faq-5">aqui</a> para saber mais.',            
+            'fonte_1': 'URL do portal público principal que disponibiliza o diário oficial deste município.'
         }
