085439712eb7ae618642b4ee0b497ee61db2e129
Mario Sergio <mariopekenossalro@gmail.com>
2020-11-28 12:04:36 -0300

    added new view admin

--- formulario/admin.py
+++ formulario/admin.py
@@ -19,6 +19,15 @@ class MapeamentoAdmin(admin.ModelAdmin):
         qs = super(MapeamentoAdmin, self).get_queryset(request)
         return qs.filter(validacao=False)
 
+class MapeamentoCemMilhabitante(Mapeamento):
+    class Meta:
+        proxy = True
+
+class MapeamentoCemMilhabitanteAdmin(MapeamentoAdmin):
+    def get_queryset(self, request):
+        qs = super(MapeamentoAdmin, self).get_queryset(request)
+        return qs.filter(municipio__populacao_2020__gt=100000, validacao=False)
+
 class MapeamentoValidado(Mapeamento):
     class Meta:
         proxy = True
@@ -49,6 +58,7 @@ class MapeamentoValidadoCapitaiAdmin(MapeamentoAdmin):
 
 admin.site.register(Municipio, MunicipioAdmin)
 admin.site.register(Mapeamento, MapeamentoAdmin)
+admin.site.register(MapeamentoCemMilhabitante, MapeamentoCemMilhabitanteAdmin)
 admin.site.register(MapeamentoValidado, MapeamentoValidadoAdmin)
 admin.site.register(MapeamentoValidadoCemMilHabitante, MapeamentoValidadoCemMilHabitanteAdmin)
 admin.site.register(MapeamentoValidadoCapitai, MapeamentoValidadoCapitaiAdmin)
