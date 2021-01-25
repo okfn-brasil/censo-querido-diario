66c54d863ed6d8716283a84269711bb6387bfa83
bcbernardo <bc.bernardo@hotmail.com>
2021-01-25 07:59:52 -0300

    Get portals using Querido Diario Census as source

---
+++ utils/src/fetch_portals/test/test_sources.py
@@ -0,0 +1,21 @@
+# Copyright 2020 Open Knowledge Brasil
+
+# Use of this source code is governed by an MIT-style
+# license that can be found in the LICENSE file or at
+# https://opensource.org/licenses/MIT.
+
+
+from ..sources import get_portals_from_census
+from ...models import Portal, PortalList
+
+
+def test_get_portals_from_census() -> None:
+    """Test getting a list of official gazettes portals from the QD census.
+    """
+    portals = get_portals_from_census()
+    assert len(portals) >= 326  # there are at least 324 mapped portals
+    assert isinstance(portals, PortalList)
+    for portal in portals:
+        assert isinstance(portal, Portal)
+        # assert len(portal.ibge_code) == 7
+        assert len(portal.url.domain) > 5
