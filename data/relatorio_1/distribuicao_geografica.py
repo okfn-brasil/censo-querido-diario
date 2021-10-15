f17e50e5858c15165e344e84892fc054e39eb419
Juliana Trevine <44185775+trevineju@users.noreply.github.com>
2021-10-15 16:10:12 -0300

    Update distribuicao_geografica.py

--- data/relatorio_1/distribuicao_geografica.py
+++ data/relatorio_1/distribuicao_geografica.py
@@ -14,14 +14,8 @@ def exporta(dados, nome):
 def municipios_UF(df):
     municipios_por_uf = df['UF'].value_counts().sort_values()
     exporta(municipios_por_uf, "Municipios-por-UF")
-    
-    municipios_por_ufp =  df['UF'].value_counts()/municipios_por_uf.sum()*100
-    exporta(municipios_por_ufp, "Municipios-por-UF-(%)")
-    
+      
 def municipios_regiao(df):
     municipios_por_regiao = df['regiao'].value_counts().sort_values()
     exporta(municipios_por_regiao, "Municipios-por-regiao")
-    
-    municipios_por_regiaop =  df['regiao'].value_counts()/municipios_por_regiao.sum()*100
-    exporta(municipios_por_regiaop, "Municipios-por-regiao-(%)")
-    
+   
