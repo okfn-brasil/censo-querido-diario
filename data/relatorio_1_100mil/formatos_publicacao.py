fcf2acd71ffc340b3147a0bdf646d6da2aa9b38d
Juliana <julianabtrevine@gmail.com>
2021-10-18 15:17:16 -0300

    atualiza publica HTML

--- data/relatorio_1_100mil/formatos_publicacao.py
+++ data/relatorio_1_100mil/formatos_publicacao.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 
-import pandas as pd 
+import pandas as pd
 
 def exporta(dados, nome):
     dados.to_csv(path_or_buf="./%s.csv" %(nome), encoding='UTF-8')
-    
+
 
 def formatos_publicacao(df):
     aux_abs = df["tipo_arquivo"].value_counts().sort_values()
@@ -12,14 +12,12 @@ def formatos_publicacao(df):
 
 
 def publica_HTML(df):
-    municipios_html = df[df['tipo_arquivo'] == 'HTML'].filter(items=['municipio', 'UF', 'eh_capital','dominio'])
+    municipios_html = df[df['tipo_arquivo'] == 'HTML'].filter(items=['municipio', 'data_inicial']).sort_values(by='data_inicial').head(10)
     exporta(municipios_html, "HTML")
-       
-        
+
+
 def formatos_UF(df):
     df_uf = df.groupby(["UF", "tipo_arquivo"])["municipio"].nunique()
     df_uf = df_uf.reset_index().pivot('UF', 'tipo_arquivo', 'municipio')
     df_uf = df_uf.fillna(0).astype(int)
-    exporta(df_uf, "Formatos-UF")    
-
-
+    exporta(df_uf, "Formatos-UF")
