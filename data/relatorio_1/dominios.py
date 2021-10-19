a08e3e90e5f57925d28784e467e8893c2f192c0b
Juliana <julianabtrevine@gmail.com>
2021-10-19 17:40:32 -0300

    Corrige dominios.py
    - Deleta encoding
    - Altera o caminho de exportação de dados
    - Renomeia arquivos exportados
    - Corrige nome da função para deixar em caixa-baixa
    - Deleta a função publica_HTML

--- data/relatorio_1/dominios.py
+++ data/relatorio_1/dominios.py
@@ -1,47 +1,39 @@
-
-# -*- coding: utf-8 -*-
-
 import pandas as pd
 
 def exporta(dados, nome):
-    dados.to_csv(path_or_buf="./%s.csv" %(nome), encoding='UTF-8')
-    
-    
+    dados.to_csv(f"./resultados/{nome}.csv", encoding='UTF-8')
+
+
 def dominio(df):
     temp = df['dominio'].value_counts().sort_values(ascending=False)
-    exporta(temp, "Dominio")       
+    exporta(temp, "(c) Dominio")
 
-    
-def dominio_UF(df):
+
+def dominio_por_uf(df):
     temp = df.groupby(["dominio"])["UF"].nunique().sort_values(ascending=False)           # total de estados atendidos pelo domínio
-    exporta(temp, "Dominio_UF")   
+    exporta(temp, "(c) Dominio_por_UF")
 
-    
-def publica_HTML(df):
-    municipios_html = df[df['tipo_arquivo'] == 'HTML'].filter(items=['municipio', 'UF', 'eh_capital','dominio'])
-    exporta(municipios_html, "HTML_dominio")
-    
-    
-def is_gov(df):
+
+def eh_gov(df):
     govern = df[df["fonte"].str.contains("gov.br", na=False)].shape[0]
     n_govern = df.shape[0] - govern
     temp = pd.DataFrame({"Fonte Governamental": [govern], "Fonte não-Governamental": [n_govern]}).transpose()
     temp.columns = ['Quantidade']
-    exporta(temp, "PortaisGOV")
-    
-    
-def is_https(df):
+    exporta(temp, "(c) PortaisGOV")
+
+
+def eh_https(df):
     https = df[df["fonte"].str.contains("https", na=False)].shape[0]
     http = df.shape[0] - https
     temp = pd.DataFrame({"HTTP": [http], "HTTPS": [https]}).transpose()
     temp.columns = ['Quantidade']
-    exporta(temp, "HTTPs")
+    exporta(temp, "(c) HTTPs")
 
-    
-def qntd_fontes(df):
+
+def quantidade_fontes(df):
     um = df[df['qtd_fontes'] == 1].shape[0]
     dois = df[df['qtd_fontes'] == 2].shape[0]
     tres = df[df['qtd_fontes'] == 3].shape[0]
-    temp = pd.DataFrame({"1": [um], "2": [dois], "3": [tres]}).transpose()
+    temp = pd.DataFrame({"Uma fonte": [um], "Duas fontes": [dois], "Três fontes": [tres]}).transpose()
     temp.columns = ["Contagem de cidades"]
-    exporta(temp, "quantidade-fontes")
+    exporta(temp, "(c) quantidade-fontes")
