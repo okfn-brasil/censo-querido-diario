aea7f2fc2ddb9c7924f3029e7a5614546e3076d9
Juliana <julianabtrevine@gmail.com>
2021-10-19 17:40:56 -0300

    Corrige formatos_publicacao.py
    - Deleta encoding
    - Altera o caminho de exportação de dados
    - Renomeia arquivos exportados
    - Corrige nome da função para deixar em caixa-baixa

--- data/relatorio_1/formatos_publicacao.py
+++ data/relatorio_1/formatos_publicacao.py
@@ -1,25 +1,21 @@
-# -*- coding: utf-8 -*-
-
-import pandas as pd 
+import pandas as pd
 
 def exporta(dados, nome):
-    dados.to_csv(path_or_buf="./%s.csv" %(nome), encoding='UTF-8')
-    
+    dados.to_csv(f"./resultados/{nome}.csv", encoding='UTF-8')
+
 
 def formatos_publicacao(df):
     aux_abs = df["tipo_arquivo"].value_counts().sort_values()
-    exporta(aux_abs, "Formatos-publicacao")
+    exporta(aux_abs, "(b) Formatos-publicacao")
 
 
-def publica_HTML(df):
-    municipios_html = df[df['tipo_arquivo'] == 'HTML'].filter(items=['municipio', 'UF', 'eh_capital','dominio'])
-    exporta(municipios_html, "HTML")
-       
-        
-def formatos_UF(df):
+def publica_html(df):
+    municipios_html = df[df['tipo_arquivo'] == 'HTML'][['municipio', 'data_inicial']].sort_values('data_inicial').head(10)
+    exporta(municipios_html, "(b) TOP10-HTML")
+
+
+def formatos_por_uf(df):
     df_uf = df.groupby(["UF", "tipo_arquivo"])["municipio"].nunique()
     df_uf = df_uf.reset_index().pivot('UF', 'tipo_arquivo', 'municipio')
     df_uf = df_uf.fillna(0).astype(int)
-    exporta(df_uf, "Formatos-UF")    
-
-
+    exporta(df_uf, "(b) Formatos-UF")
