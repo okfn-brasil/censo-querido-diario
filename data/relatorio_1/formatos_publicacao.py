bdd502283341a798014c651e310275782b870584
Juliana <julianabtrevine@gmail.com>
2021-10-22 15:54:40 -0300

    deleta função não mais utilizada

--- data/relatorio_1/formatos_publicacao.py
+++ data/relatorio_1/formatos_publicacao.py
@@ -12,10 +12,3 @@ def formatos_publicacao(df):
 def publica_html(df):
     municipios_html = df[df['tipo_arquivo'] == 'HTML'][['municipio', 'data_inicial']].sort_values('data_inicial').head(10)
     exporta(municipios_html, "(b) TOP10-HTML")
-
-
-def formatos_por_uf(df):
-    df_uf = df.groupby(["UF", "tipo_arquivo"])["municipio"].nunique()
-    df_uf = df_uf.reset_index().pivot('UF', 'tipo_arquivo', 'municipio')
-    df_uf = df_uf.fillna(0).astype(int)
-    exporta(df_uf, "(b) Formatos-UF")
