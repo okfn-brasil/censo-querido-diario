34585b1d4943dbf589736583025cdd5c4a8c37aa
Juliana <julianabtrevine@gmail.com>
2021-10-22 22:25:24 -0300

    altera os módulos, corrige e deleta funções, atualiza trata_fontes

--- data/relatorio_1/preprocessamento.py
+++ data/relatorio_1/preprocessamento.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import numpy as np
 from urllib.parse import urlparse
-from urllib.parse import urlsplit
-from functools import reduce
+
+
+import auxilia_tratamento_fontes
 
 def recorta_perfil(df):
     ''' (DataFrame) -> DataFrame
@@ -15,7 +16,7 @@ def substitui_sem_diario(df):
     Atualiza os dados guardados em 'is_online', 'data_inicial', 'tipo_arquivo' em 4 cidades para 'Indefinido'.
     '''
     cidades=["Viamão (RS)", "Rio Grande (RS)", "Tubarão (SC)", "Tatuí (SP)"]
-    variaveis=["is_online", "data_inicial", "tipo_arquivo"]
+    variaveis=["is_online", "data_inicial", "tipo_arquivo", 'fonte_1', 'fonte_2', 'fonte_3', 'fonte_4']
     colunas = df.columns.to_list()
 
     for cidade in cidades:
@@ -39,39 +40,6 @@ def remove_variaveis(df):
     Remove as variaveis IBGE, validacao, navegacao, observacoes
     '''
     return df.drop(columns=['IBGE', 'validacao', 'navegacao', 'observacoes'])
-
-
-def extract_domain_and_path(df):
-    '''(DataFrame) -> DataFrame
-    (((explicar o que isso faz)))
-    '''
-    return df.apply(lambda url: urlsplit(url)[1:3]).str.join('')
-
-
-def remove_www(df):
-    '''(DataFrame) -> DataFrame
-    (((explicar o que isso faz)))
-    '''
-    return df.str.replace('^www\d?\.', '')
-
-
-def trata_fontes(df):
-    ''' (DataFrame) -> DataFrame
-    Percorre as variaveis de fontes e cria um novo DataFrame com entradas para cada fonte e não para cada cidade
-    '''
-    fonte_melted = df.melt(value_vars=['fonte_1', 'fonte_2', 'fonte_3', 'fonte_4'], value_name='fonte', ignore_index=False)
-    df = df.join(fonte_melted['fonte']).drop(columns=['fonte_1', 'fonte_2', 'fonte_3', 'fonte_4'])
-    df = df[df['fonte'] != 'None'].reset_index(drop=True)
-
-    # cria a variavel dominio para a lista de fontes
-    df['dominio'] = df['fonte'].apply(lambda url: urlparse(url).netloc)
-
-    # cria a variavel path
-    df['fonte_dominio_path'] = df['fonte'] \
-                .pipe(extract_domain_and_path) \
-                .pipe(remove_www) \
-                .str.rstrip('/')
-    return df
 
 
 def trata_datas(df):
@@ -98,3 +66,52 @@ def conta_fontes(df, df_aux):
     s.columns=["qtd_fontes"]
     df = df.join(s, on = 'municipio', how='left')
     return df
+
+
+def trata_fontes(fontes):
+    ''' (DataFrame) -> DataFrame
+    Percorre as variaveis de fontes e cria um novo DataFrame com entradas para cada fonte e não para cada cidade
+    '''
+    # cria conjunto de dados por fonte
+    fontes = auxilia_tratamento_fontes.fontes_melted(fontes) 
+
+    # cria a variavel dominio para a lista de fontes
+    fontes['dominio'] = fontes['fonte'].apply(lambda url: urlparse(url).netloc)
+
+    # cria a variavel path
+    fontes = auxilia_tratamento_fontes.extrai_path(fontes)
+                    
+    '''
+    A partir da variavel path, é possivel perceber que apenas 20 delas se repetem
+    Essas 20 são classificadas manualmente a seguir
+    '''
+    referencia_classificacao = auxilia_tratamento_fontes.cria_classificacao()
+    
+    #utiliza a referencia_classificacao para classificar as demais fontes
+    fontes = auxilia_tratamento_fontes.classifica_fontes(fontes, referencia_classificacao)
+    
+    # remove stopwords (e, de), remove caracteres especiais, acentos, fazer o nome ficar em caixa baixa
+    fontes = auxilia_tratamento_fontes.corrige_palavras(fontes)
+    
+    #lida com o conflito de trechos de palavras estarem contidas em outras (diaRIOs, reSULtados)
+    fontes = auxilia_tratamento_fontes.conflito_em_palavras(fontes)
+    
+    #atualiza a referencia de classificacao com as novas fontes municipais encontradas
+    referencia_classificacao['fontes_municipio'].extend(fontes[fontes['municipio_has_name_word_in_fonte']]['fonte_dominio_path'].to_list())
+    
+    #classifica os municipios que sao de fonte compartilhada
+    fontes_compartilhadas = referencia_classificacao["fontes_associacao"] + referencia_classificacao["fontes_estado"] + referencia_classificacao["fontes_uniao"]
+    fontes["fontes_compartilhadas"] = fontes['fonte'].apply(lambda fonte: any([fc in fonte for fc in fontes_compartilhadas]))
+    
+    #classifica as fontes restantes, as não compartilhadas
+    referencia_classificacao = auxilia_tratamento_fontes.estende_classificacao(referencia_classificacao)
+    
+    #classifica fontes
+    fontes = auxilia_tratamento_fontes.classifica_fontes(fontes, referencia_classificacao)
+    
+    #classifica fontes gov
+    fontes['fonte_governamental'] = fontes['dominio'].str.contains('\.gov')
+    
+    return fontes
+
+
